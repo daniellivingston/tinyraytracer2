0521eab639e2d11ecbb51681b3435cfa970289e2
Daniel Reece Livingston <livingston@lanl.gov>
2019-02-07 18:23:58 -0700

    Beginning of Rust...cool

--- c++/main.cpp
+++ c++/main.cpp
@@ -5,6 +5,32 @@
 #include <vector>
 #include "geometry.h"
 
+/*
+struct Sphere {
+    Vec3f center;
+    float radius;
+
+    Sphere(const Vec3f &c, const float &r) : center(c), radius(r) {}
+
+    // Method to determine if ray intersects sphere
+    bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0) const {
+        Vec3f L = center - orig;
+        float tca = L*dir;
+        float d2 = L*L - tca*tca;
+
+        if (d2 > radius*radius) return false;
+
+        float thc = sqrtf(radius*radius - d2);
+        t0 = tca - thc;
+        float t1 = tca + thc;
+
+        if (t0 < 0) t0 = t1;
+        if (t0 < 0) return false;
+        return true;
+    }
+};
+*/
+
 void render() {
 
     // Define the domain of the image to render
